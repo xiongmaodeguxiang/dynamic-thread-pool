7b4bb21690009d8fc16eff5f53df71409748ae56
zhanglin574 <zhanglin574@xdf.cn>
2024-05-23 14:19:56 +0800

    change gauge label

--- src/main/java/com/zl/learn/threads/monitor/ExecutorGauge.java
+++ src/main/java/com/zl/learn/threads/monitor/ExecutorGauge.java
@@ -21,17 +21,17 @@ public class ExecutorGauge {
         this.executorName = executorName;
         this.applicationName = applicationName;
         this.collectorRegistry = collectorRegistry;
-        coreGauge = Gauge.build().name("executor_monitor").labelNames("application", "executor_name","type")
+        coreGauge = Gauge.build().name("core_size_monitor").labelNames("application", "executor_name")
                 .help("core size monitor").register(collectorRegistry);
-        maxGauge = Gauge.build().name("executor_monitor").labelNames("application", "executor_name","type")
+        maxGauge = Gauge.build().name("max_size_monitor").labelNames("application", "executor_name")
                 .help("max size monitor").register(collectorRegistry);
-        activeGauge = Gauge.build().name("executor_monitor").labelNames("application", "executor_name","type")
+        activeGauge = Gauge.build().name("active_size_monitor").labelNames("application", "executor_name")
                 .help("active size monitor").register(collectorRegistry);
-        poolGauge = Gauge.build().name("executor_monitor").labelNames("application", "executor_name","type")
+        poolGauge = Gauge.build().name("pool_size_monitor").labelNames("application", "executor_name")
                 .help("pool size monitor").register(collectorRegistry);
-        largestGauge = Gauge.build().name("executor_monitor").labelNames("application", "executor_name","type")
+        largestGauge = Gauge.build().name("largest_size_monitor").labelNames("application", "executor_name")
                 .help("largest size monitor").register(collectorRegistry);
-        queueGauge = Gauge.build().name("executor_monitor").labelNames("application", "executor_name","type")
+        queueGauge = Gauge.build().name("queue_size_monitor").labelNames("application", "executor_name")
                 .help("queue size monitor").register(collectorRegistry);
     }
 
@@ -45,27 +45,27 @@ public class ExecutorGauge {
     }
 
     private void gaugeQueue(int size) {
-        queueGauge.labels(applicationName, executorName,"queue_size").set(size);
+        queueGauge.labels(applicationName, executorName).set(size);
     }
 
     private void gaugeLargest(int largestPoolSize) {
-        largestGauge.labels(applicationName, executorName,"largest_size").set(largestPoolSize);
+        largestGauge.labels(applicationName, executorName).set(largestPoolSize);
     }
 
     private void gaugePool(int poolSize) {
-        poolGauge.labels(applicationName, executorName,"pool_size").set(poolSize);
+        poolGauge.labels(applicationName, executorName).set(poolSize);
     }
 
     private void gaugeActive(int activeCount) {
-        activeGauge.labels(applicationName, executorName,"active_size").set(activeCount);
+        activeGauge.labels(applicationName, executorName).set(activeCount);
     }
 
     private void gaugeMax(int maximumPoolSize) {
-        maxGauge.labels(applicationName, executorName,"max_size").set(maximumPoolSize);
+        maxGauge.labels(applicationName, executorName).set(maximumPoolSize);
     }
 
     private void gaugeCore(int corePoolSize) {
-        coreGauge.labels(applicationName, executorName,"core_size").set(corePoolSize);
+        coreGauge.labels(applicationName, executorName).set(corePoolSize);
     }
 
 }
