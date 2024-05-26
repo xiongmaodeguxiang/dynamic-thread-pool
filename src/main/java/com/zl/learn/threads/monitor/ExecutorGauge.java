ce6b331b73f66ade5af47f5517e9f200497696a4
zhanglin574 <zhanglin574@xdf.cn>
2024-05-26 09:17:07 +0800

    fix gauge

--- src/main/java/com/zl/learn/threads/monitor/ExecutorGauge.java
+++ src/main/java/com/zl/learn/threads/monitor/ExecutorGauge.java
@@ -1,5 +1,6 @@
 package com.zl.learn.threads.monitor;
 
+import com.zl.learn.threads.executor.DynamicThreadPoolExecutor;
 import io.prometheus.client.CollectorRegistry;
 import io.prometheus.client.Gauge;
 
@@ -7,9 +8,7 @@ import java.util.concurrent.Executor;
 import java.util.concurrent.ThreadPoolExecutor;
 
 public class ExecutorGauge {
-    private String executorName;
     private CollectorRegistry collectorRegistry;
-    private String applicationName;
     private Gauge coreGauge;
     private Gauge maxGauge;
     private Gauge activeGauge;
@@ -17,55 +16,54 @@ public class ExecutorGauge {
     private Gauge largestGauge;
     private Gauge queueGauge;
 
-    public ExecutorGauge(String executorName, CollectorRegistry collectorRegistry, String applicationName){
-        this.executorName = executorName;
-        this.applicationName = applicationName;
+    public ExecutorGauge(CollectorRegistry collectorRegistry){
         this.collectorRegistry = collectorRegistry;
-        coreGauge = Gauge.build().name("core_size_monitor").labelNames("application", "executor_name")
+        coreGauge = Gauge.build().name("core_size_monitor").labelNames("executor_name")
                 .help("core size monitor").register(collectorRegistry);
-        maxGauge = Gauge.build().name("max_size_monitor").labelNames("application", "executor_name")
+        maxGauge = Gauge.build().name("max_size_monitor").labelNames( "executor_name")
                 .help("max size monitor").register(collectorRegistry);
-        activeGauge = Gauge.build().name("active_size_monitor").labelNames("application", "executor_name")
+        activeGauge = Gauge.build().name("active_size_monitor").labelNames("executor_name")
                 .help("active size monitor").register(collectorRegistry);
-        poolGauge = Gauge.build().name("pool_size_monitor").labelNames("application", "executor_name")
+        poolGauge = Gauge.build().name("pool_size_monitor").labelNames("executor_name")
                 .help("pool size monitor").register(collectorRegistry);
-        largestGauge = Gauge.build().name("largest_size_monitor").labelNames("application", "executor_name")
+        largestGauge = Gauge.build().name("largest_size_monitor").labelNames("executor_name")
                 .help("largest size monitor").register(collectorRegistry);
-        queueGauge = Gauge.build().name("queue_size_monitor").labelNames("application", "executor_name")
+        queueGauge = Gauge.build().name("queue_size_monitor").labelNames("executor_name")
                 .help("queue size monitor").register(collectorRegistry);
     }
 
-    public void gauge(ThreadPoolExecutor executor){
-        gaugeCore(executor.getCorePoolSize());
-        gaugeMax(executor.getMaximumPoolSize());
-        gaugeActive(executor.getActiveCount());
-        gaugePool(executor.getPoolSize());
-        gaugeLargest(executor.getLargestPoolSize());
-        gaugeQueue(executor.getQueue().size());
+    public void gauge(DynamicThreadPoolExecutor executor){
+        String businessName = executor.getBusinessName();
+        gaugeCore(businessName,executor.getCorePoolSize());
+        gaugeMax(businessName,executor.getMaximumPoolSize());
+        gaugeActive(businessName,executor.getActiveCount());
+        gaugePool(businessName,executor.getPoolSize());
+        gaugeLargest(businessName,executor.getLargestPoolSize());
+        gaugeQueue(businessName,executor.getQueue().size());
     }
 
-    private void gaugeQueue(int size) {
-        queueGauge.labels(applicationName, executorName).set(size);
+    private void gaugeQueue(String executorName, int size) {
+        queueGauge.labels(executorName).set(size);
     }
 
-    private void gaugeLargest(int largestPoolSize) {
-        largestGauge.labels(applicationName, executorName).set(largestPoolSize);
+    private void gaugeLargest(String executorName, int largestPoolSize) {
+        largestGauge.labels(executorName).set(largestPoolSize);
     }
 
-    private void gaugePool(int poolSize) {
-        poolGauge.labels(applicationName, executorName).set(poolSize);
+    private void gaugePool(String executorName, int poolSize) {
+        poolGauge.labels(executorName).set(poolSize);
     }
 
-    private void gaugeActive(int activeCount) {
-        activeGauge.labels(applicationName, executorName).set(activeCount);
+    private void gaugeActive(String executorName, int activeCount) {
+        activeGauge.labels(executorName).set(activeCount);
     }
 
-    private void gaugeMax(int maximumPoolSize) {
-        maxGauge.labels(applicationName, executorName).set(maximumPoolSize);
+    private void gaugeMax(String executorName, int maximumPoolSize) {
+        maxGauge.labels(executorName).set(maximumPoolSize);
     }
 
-    private void gaugeCore(int corePoolSize) {
-        coreGauge.labels(applicationName, executorName).set(corePoolSize);
+    private void gaugeCore(String executorName, int corePoolSize) {
+        coreGauge.labels(executorName).set(corePoolSize);
     }
 
 }
